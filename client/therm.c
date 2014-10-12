e90a071d8fe984313908c0637284dcafeb110f88
Nathan V <nvahren@ubuntu.ubuntu-domain>
2014-10-12 14:00:41 -0400

    Socket/connection added

--- client/therm.c
+++ client/therm.c
@@ -3,11 +3,65 @@ Chris Ray
 Nathan Vahrenberg
 
 CSE30264 - Computer Networks
-Project 2 - Thermal Sensor Server
+Project 2 - Thermal Sensor Client
 */
 
-int main(){
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
 
+#include <errno.h>
+
+#define DPORT 9765
+
+int main( int argc, char *argv[] ){
+
+	int sockfd, port;
+	struct sockaddr_in serverAddr;
+	struct hostent *hostname;
+	
+	int exit = 0;
+	
+	// Open socket
+	if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ){
+		perror( "Unable to open socket" ); return 0;
+	}
+	
+	// Get host address from 1st arg
+	if( (hostname = gethostbyname(argv[1])) == 0 ){
+		perror( "Unable to get address of host"); return 0;
+	}
+	
+	// Set port number
+	if( argc >= 3 ){
+		port = atoi(argv[2]);
+	}else{
+		port = DPORT;
+	}
+	
+	// Set server address information
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port);
+	memcpy((void *)&serverAddr.sin_addr, hostname->h_addr_list[0], hostname->h_length);
+	
+	// Connect to host
+	if( (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr))) < 0 ){
+		perror("Unable to connect to host"); return 0;
+	}
+	
+	#ifdef DEBUG
+		printf("Connected to host %s (%s) on port %d\n",argv[1],inet_ntoa(serverAddr.sin_addr),port);
+	#endif
+	
+	// Read sensor info and send to host
 
 }
--- server/thermd.c
+++ server/thermd.c
@@ -3,11 +3,79 @@ Chris Ray
 Nathan Vahrenberg
 
 CSE30264 - Computer Networks
-Project 2 - Thermal Sensor Client
+Project 2 - Thermal Sensor Server
 */
 
-int main(){
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+
+#include <errno.h>
+
+#define DPORT 9765
+
+int main( int argc, char *argv[] ){
+
+	int sockfd, port, conn;
+	struct sockaddr_in clientAddr, serverAddr;
+	socklen_t csize = sizeof(clientAddr);
+	
+	int exit = 0;
+	
+	// Open socket
+	if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ){
+		perror("Unable to open socket"); return 0;
+	}
+	
+	// Set port number
+	if( argc >= 2 ){
+		port = atoi(argv[1]);
+	}else{
+		port = DPORT;
+	}
+	
+	// Set server address info
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serverAddr.sin_port=htons(port);
+	
+	// Set socket options
+	int opt = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(int));
+	
+	// Bind to socket
+	if( (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr))) < 0 ){
+		perror("Unable to bind"); return 0;
+	}
+	
+	// Begin accepting connections to socket
+	if( (listen(sockfd, 1024)) < 0 ){
+		perror("Unable to accept connections"); return 0;
+	}
+	
+	#ifdef DEBUG
+		printf("Accepting connections on port %d\n",port);
+	#endif
+	
+	// Begin loop and wait for client connection
+	while(!exit){
+	
+		// Accept a connection
+		conn = accept(sockfd, (struct sockaddr *)&clientAddr, &csize);
+	
+		#ifdef DEBUG
+			printf("Client connected from %s\n",inet_ntoa(clientAddr.sin_addr));
+		#endif
+		
+		// Get sensor info from client
+	
+	}
 
 
 }
